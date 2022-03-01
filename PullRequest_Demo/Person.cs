3d454f288fa7005d56d46c9e81528274130463af
Lion1Blue <lionhillebrand@gmx.de>
2022-03-01 08:10:05 +0100

    added Person class

---
+++ PullRequest_Demo/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PullRequest_Demo
+{
+    class Person
+    {
+        public string Vorname { get; private set; }
+        public string Nachname { get; private set; }
+        public int Alter { get; private set; }
+
+        List<Person> freunde = new List<Person>();
+
+        public Person(int alter, string vorname, string nachname)
+        {
+            Alter = alter;
+            Vorname = vorname;
+            Nachname = nachname;
+        }
+
+        public void AddFreund(Person person)
+        {
+            freunde.Add(person);
+        }
+    }
+}
