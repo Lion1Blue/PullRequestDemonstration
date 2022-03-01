6ca6af58f18d12af9bd3905d44348a8c3ba8027b
Lion1Blue <lionhillebrand@gmx.de>
2022-03-01 10:59:22 +0100

    added House class

---
+++ PullRequest_Demo/House.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PullRequest_Demo
+{
+    class House
+    {
+        public string Adress { get; private set; }
+        List<Person> residents = new List<Person>();
+
+        public House(List<Person> residents, string adress)
+        {
+            Adress = adress;
+            this.residents = residents;
+        }
+
+        public void AddResident(Person resident)
+        {
+            residents.Add(resident);
+        }
+
+        public bool RemoveResident(Person resident)
+        {
+            return residents.Remove(resident);
+        }
+    }
+}
