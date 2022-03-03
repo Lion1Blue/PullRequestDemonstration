7b074565d7a22f91422da9241108ce3676101c9f
Lion1Blue <lionhillebrand@gmx.de>
2022-03-03 07:43:09 +0100

    added Cat Class, added doc-Pictures

---
+++ PullRequest_Demo/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PullRequest_Demo
+{
+    class Cat : Pet
+    {
+        
+
+    }
+}
